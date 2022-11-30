c60f9c4c03249b2c5b38de8f333a7b62bc4e9b21
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2022-11-30 11:57:05 -0300

    queen moves now is an array, and moves goes until first piece block

--- lib/queen.rb
+++ lib/queen.rb
@@ -12,14 +12,7 @@ class Queen
   include LinePiece
 
   def moves
-    {
-      vertical_moves: vertical_moves,
-      horizontal_moves: horizontal_moves,
-      top_left: top_left,
-      top_right: top_right,
-      bottom_left: bottom_left,
-      bottom_right: bottom_right
-    }
+    (vertical_moves + horizontal_moves + top_left + top_right + bottom_left + bottom_right).sort
   end
 
   private
