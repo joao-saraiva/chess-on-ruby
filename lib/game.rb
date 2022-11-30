c1ca311324b029089468423f561b0fb0946f87b4
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2022-11-30 15:30:59 -0300

    visual imporvments and fuxing a bug to seek eminies on vertical movents, start to search enemies by reverse letters

---
+++ lib/game.rb
@@ -0,0 +1,36 @@
+require_relative 'chess_board'
+require 'byebug'
+
+@chess_board = ChessBoard.new
+
+until @chess_board.over?
+  puts @chess_board.draw_board
+
+  puts "Select a piece"
+  selected_position = gets.chomp
+
+  @chess_board.selected_position = selected_position
+  begin 
+    @chess_board.position_formated_correctly?
+    @chess_board.selected_piece_on_turn?
+
+    system "clear"
+    puts @chess_board.draw_board(@chess_board.selected_piece_avaliable_moves)
+    puts "Select a move"
+    new_position = gets.chomp
+
+    raise "Move is not avaliable" unless @chess_board.selected_piece_avaliable_moves.include?(new_position)
+
+    if @chess_board.king_on_check?
+      player_is_moving_the_king = @chess_board.selected_piece.signature == "K"
+      @chess_board.king_still_on_check_with_move?(player_is_moving_the_king ? new_position : nil)
+    end
+
+    system "clear"
+    @chess_board.move_piece(new_position)
+  rescue => e
+    system "clear"
+    puts e.message
+    next
+  end
+end
